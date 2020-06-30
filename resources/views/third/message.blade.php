fb72ec0f022b83164e70f186aeae49e8ed6e11bf
ookawa0602 <forefront.ookawa2@gmail.com>
2020-06-30 15:33:15 +0900

    message.blade.php commit

---
+++ resources/views/third/message.blade.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="{{ asset('/css/components.css')}}">
+ </head>
+ <body>
+  <div class="message">
+    <p class="msg_title">{{$msg_title}}</p>
+    <p class="msg_content">{{$msg_content}}</p>
+ </body>
+</html>
